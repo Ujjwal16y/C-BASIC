9b8948898d64d817f43cc55a7bb1e681c96f54d2
Ujjwal Yadav <86011205+Ujjwal16y@users.noreply.github.com>
2021-06-25 09:13:05 +0530

    Add files via upload

---
+++ MAX. OF FOUR NUMBER.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+/*
+Add `int max_of_four(int a, int b, int c, int d)` here.
+*/
+int max_of_four(int a, int b, int c, int d)
+{
+    int largest,smallest;
+    largest =a;
+
+    if ( largest < b )
+    {
+        largest = b;
+    }
+
+    if ( largest < c )
+    {
+        largest = c;
+
+    }
+    if ( largest < d )
+    {
+        largest = d;
+    }
+
+    return largest;
+}
+int main()
+ {
+    int a, b, c, d;
+    scanf("%d %d %d %d", &a, &b, &c, &d);
+    int ans = max_of_four(a, b, c, d);
+    printf("%d", ans);
+
+    return 0;
+}
